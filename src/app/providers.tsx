a231f3e8dc491bf517545382bcfb46cd4a3be3d2
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2024-06-29 01:39:21 +0400

    feat: Add QueryClientProvider to Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,13 @@
 "use client";
+import QUERY_CLIENT from "@/constants/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
 export default function Providers({ children }: ProvidersProps) {
-  return children;
+  return (
+    <QueryClientProvider client={QUERY_CLIENT}>{children}</QueryClientProvider>
+  );
 }
---
+++ src/constants/query-client.ts
@@ -0,0 +1,5 @@
+import { QueryClient } from "@tanstack/react-query";
+
+const QUERY_CLIENT = new QueryClient();
+
+export default QUERY_CLIENT;
