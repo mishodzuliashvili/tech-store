2726f93698a450f9390bc0e88712aa5ff50779eb
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2024-06-30 19:24:11 +0400

    refactor: Update navbar component to improve responsiveness and add cart functionality

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,48 +1,68 @@
+"use client";
 import COMPANY from "@/constants/company";
 import SearchBox from "./search-box";
+import { FiSearch, FiShoppingCart } from "react-icons/fi";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import AccountNavbarBlock from "./account-navbar-block";
+import { useEffect, useState } from "react";
+import { useCartStore } from "@/store/cart";
+import useIsMounted from "@/hooks/is-mounted";
 
 type NavbarProps = {};
 
 export default function Navbar({}: NavbarProps) {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const { count } = useCartStore();
+  const isMounted = useIsMounted();
+
+  if (!isMounted) return null;
+
   return (
-    <header className="flex flex-wrap w-full bg-white text-sm py-4">
-      <div className="container">
-        <nav className="w-full sm:flex sm:items-center gap-5">
-          <div className="flex items-center justify-between">
-            <a
-              className="inline-flex w-[240px] items-center gap-x-2 text-xl font-semibold"
-              href="#"
-            >
-              <div className="rounded bg-primary h-[40px] w-[40px] flex items-center justify-center text-white">
-                {COMPANY.title[0]}
-              </div>
-              <span className="whitespace-nowrap">{COMPANY.title}</span>
-            </a>
-          </div>
-          <div className="w-full">
+    <header className="flex flex-wrap w-full bg-white text-sm py-6">
+      <div className="container  bg-white">
+        <nav className="flex justify-between sm:grid sm:grid-cols-[240px,1fr,auto] items-center gap-5">
+          <Link
+            className="flex items-center gap-x-2 text-xl font-semibold"
+            href="/"
+          >
+            <div className="rounded bg-primary h-[40px] w-[40px] flex items-center justify-center text-primary-foreground">
+              {COMPANY.title[0]}
+            </div>
+            <span className="hidden sm:block whitespace-nowrap">
+              {COMPANY.title}
+            </span>
+          </Link>
+          <div className="hidden sm:block max-w-screen-sm">
             <SearchBox />
           </div>
-          <div className="hidden overflow-hidden transition-all duration-300 sm:block">
-            <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-              <a
-                className="font-medium text-primary"
-                href="#"
-                aria-current="page"
-              >
-                Landing
-              </a>
-              <a className="font-medium" href="#">
-                Account
-              </a>
-              <a className="font-medium" href="#">
-                Work
-              </a>
-              <a className="font-medium" href="#">
-                Blog
-              </a>
-            </div>
+          <div className="flex gap-5 items-center justify-end">
+            <Button
+              onClick={() => {
+                setIsSearchOpen((o) => !o);
+              }}
+              variant="ghost"
+              size="icon"
+              className="sm:hidden"
+            >
+              <FiSearch size={22} />
+            </Button>
+            <Button asChild variant="ghost" size="icon">
+              <Link className="font-medium relative" href="/cart">
+                <FiShoppingCart size={22} />
+                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-primary-foreground bg-primary border-2 border-background rounded-full -top-1 -end-1">
+                  {count()}
+                </div>
+              </Link>
+            </Button>
+            <AccountNavbarBlock />
           </div>
         </nav>
+        {isSearchOpen && (
+          <div className="mt-4 relative w-full sm:hidden">
+            <SearchBox />
+          </div>
+        )}
       </div>
     </header>
   );
