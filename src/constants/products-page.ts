2cd85913a81bdc1b5bd69ae962d23e8b8cb798c3
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2024-07-01 20:31:13 +0400

    feat: Add constant sort options to use in whole project

--- src/constants/products-page.ts
+++ src/constants/products-page.ts
@@ -2,4 +2,27 @@ const PRODUCTS_PAGE = {
   pageSize: 10,
 };
 
+export const SORT_OPTIONS = [
+  {
+    value: "price-asc",
+    label: "Price: Low to High",
+  },
+  {
+    value: "price-desc",
+    label: "Price: High to Low",
+  },
+  {
+    value: "discount-desc",
+    label: "Discount",
+  },
+  {
+    value: "newest",
+    label: "Newest",
+  },
+] as const;
+
+export const SORT_OPTIONS_VALUES = SORT_OPTIONS.map((option) => option.value);
+
+export type SortOption = (typeof SORT_OPTIONS)[number]["value"];
+
 export default PRODUCTS_PAGE;
